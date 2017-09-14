36549e3cf7321d9c416ffdb959c2dacc445a14e0
Luke Boyle <luke.boyle.au@gmail.com>
2017-09-14 23:37:38 +1000

    refactor: make the AsyncEvent class exported

---
+++ src/models/async-event.js
@@ -0,0 +1,69 @@
+/**
+ * @typedef {AsyncEvent} AsyncEvent
+ * @property {string} status
+ * @property {boolean|null} error
+ * @property {boolean} isExecuting
+ * @property {boolean} isSuccessful
+ * @property {boolean} isReady
+ */
+
+/**
+ * @returns AsyncEvent
+ */
+export default class AsyncEvent {
+	constructor() {
+		this.status = 'ready';
+		this.error = null;
+		this.isExecuting = false;
+		this.isSuccessful = false;
+		this.isReady = true;
+	}
+
+	get hasError() {
+		return this.error === true;
+	}
+
+	markAsExecuting() {
+		return {
+			...this,
+			status: 'executing',
+			error: null,
+			isExecuting: true,
+			isReady: false,
+			isSuccessful: false
+		};
+	}
+
+	resolve() {
+		return {
+			...this,
+			status: 'success',
+			error: null,
+			isExecuting: false,
+			isReady: false,
+			isSuccessful: true
+		};
+	}
+
+	reject(error) {
+		return {
+			...this,
+			status: 'error',
+			error: error,
+			isExecuting: false,
+			isReady: false,
+			isSuccessful: false
+		};
+	}
+
+	reset() {
+		return {
+			...this,
+			status: 'ready',
+			error: null,
+			isExecuting: false,
+			isReady: true,
+			isSuccessful: false
+		};
+	}
+}
